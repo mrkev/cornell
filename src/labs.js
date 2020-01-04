06cd1e715cb85c645bd7b011ac9e853c46111c42
Kevin Chavez <mr.kev@me.com>
2020-01-04 15:14:31 -0600

    prettier, lints and small fixes

--- src/labs.js
+++ src/labs.js
@@ -8,21 +8,26 @@ const parse_coords = str => {
 
 // @return array of labs
 const labs = () =>
-  request('https://mapping.cit.cornell.edu/publiclabs/map/results_as_csv.cfm')
-    .then(csv => new Promise((res, rej) => {
-      csvjson(csv, (err, output) => {
-        if (err) rej(err);
-        res(output.map(x => ({
-          id: parseInt(x[0]),
-          created: new Date(x[1]),
-          updated: new Date(x[2]),
-          name: x[3],
-          location: x[5],
-          coordinates: parse_coords(x[6]),
-          resources: x[7].split(','),
-        })));
-      });
-    })
+  request(
+    'https://mapping.cit.cornell.edu/publiclabs/map/results_as_csv.cfm'
+  ).then(
+    csv =>
+      new Promise((res, rej) => {
+        csvjson(csv, (err, output) => {
+          if (err) rej(err);
+          res(
+            output.map(x => ({
+              id: parseInt(x[0]),
+              created: new Date(x[1]),
+              updated: new Date(x[2]),
+              name: x[3],
+              location: x[5],
+              coordinates: parse_coords(x[6]),
+              resources: x[7].split(','),
+            }))
+          );
+        });
+      })
   );
 
 module.exports = { labs };
